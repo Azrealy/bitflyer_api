62e80a5ceacad31a48cdbbdfd89003bf88a76d8d
Azrealy <31608076+Azrealy@users.noreply.github.com>
2017-12-12 18:08:47 +0900

    Create Auth.py

---
+++ Auth.py
@@ -0,0 +1,17 @@
+import time 
+import hmac
+import hashlib
+
+class Authentication(object):
+    
+    def __init__(self, secret, request_path, method="GET", request_body=""):
+        self.secret = secret
+        self.method = method
+        self.request_path = request_path
+        self.request_body = request_body
+        self.access_timestamp = str(time.time())
+    
+    def signature(self):
+        api_secret = str.encode(self.secret)
+        text = str.encode(self.access_timestamp + self.method + self.request_path + self.request_body)
+        return hmac.new(api_secret, text, hashlib.sha256).hexdigest()
