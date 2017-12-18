2beaea3faa07157948295573a2a41398437d0e4b
ホウ・ガイコウ <hougaikou@hougaikounoMacBook-puro.local>
2017-12-19 07:24:51 +0900

    Add the API and the asyncorouns process of httpcline

--- bitflyerAPI.py
+++ bitflyerAPI.py
@@ -1,4 +1,4 @@
-import tornado.httpclient
+from tornado.httpclient import HTTPRequest
 import json
 from tornado.escape import json_decode, json_encode
 from tornado import gen
@@ -9,13 +9,37 @@ import urllib
 from sys import argv
 import requests
 
+
 class bitflyerApi(object):
-    
-    def __init__(self, key, secret):
+    """
+    This class can create tornado.client HTTPRequest of user 
+    to use the Bitflyer API, which website you can access follow
+    https://lightning.bitflyer.jp/docs/playground
+
+    IF you want use the private request, you should initalize the
+    key and secret of you account.
+    Example:
+    bitflyerApi(key=[YOU KEY], secret=[YOU SECRET])
+    And you can use this object to take others method to make you 
+    HTTPRequst.
+    """
+
+    def __init__(self, key=None, secret=None):
         self.key = key
         self.secret = secret
     
     def auth_header(self, request_path, method = "GET", request_body=""):
+        """
+        This function create the PRIVATE Header for specify user
+
+        Parameter
+        -------------
+        path , method , body
+        
+        Return
+        -------------
+        a authetication of the user Header.
+        """
         Auth = Authentication(self.secret, request_path, method, request_body)
         return {
         "ACCESS-KEY": self.key,
@@ -23,35 +47,137 @@ class bitflyerApi(object):
         "ACCESS-SIGN": Auth.signature(),
         "Content-Type": "application/json"
         }
+
     def request(self, request_path, method = "GET", param=None):
+        """
+        THis method can use to create HTTPRequest, which is
+        base on you need.
+
+        Parameter:
+        --------------
+        path, method, param
+
+        Return:
+        --------------
+        HTTPRequest 
+        """
         api_url = "https://api.bitflyer.jp"
-        #if param:
-            #body = json.dumps(param)
-            #print(body)
-        if method == "GET":
-            if param:
-                body = "?" + urllib.parse.urlencode(param)
-                print(request_path)
-                header = self.auth_header(request_path, request_body = body)
-            else:
-                header = self.auth_header(request_path)
+        body = ""
+        header = None
+
         if method == "POST":
+            body = json.dumps(param)
+            print(body)
+        else:
+            if param:
+                body += "?" + urllib.parse.urlencode(param)
+
+        if self.key and self.secret:
             header = self.auth_header(request_path, method, body)
-        with requests.Session() as s:
-            s.headers.update(header)
-            endpoint = api_url + request_path
-            if method == "GET":
-                response = s.get(endpoint, params=param)
-            else:
-                response = s.post(endpoint, data = body)
-            content = json.loads(response.content.decode("utf-8"))
-        print(content)
+
+        if method == "GET":
+            endpiont = api_url + request_path + body
+            return HTTPRequest(endpiont, method = method, headers = header)
+        else:
+            endpiont = api_url + request_path
+            return HTTPRequest(endpiont, method = method, headers = header, 
+                               body = body)
 
     def get_markets(self):
+        """
+        GET Public HTTPRequest
+        show the exchange markets product_code
+
+        Return
+        --------------
+        HTTPRequest of tornado.httpclient 
+        """
         request_path = "/v1/getmarkets"
         return self.request(request_path)
 
-    def send_order(self, product_code, price, size, minute_to_expire=100000, child_order_type="LIMIT", side="BUY", time_in_force="GTC"):
+    def get_ticker(self, **param):
+        """
+        GET Public HTTPRequest
+        get the ticker of exchange
+
+        Parameter:
+        -------------
+        product_code: FX_BTC_JPY
+
+        Return
+        --------------
+        HTTPRequest of tornado.httpclient
+        """
+        request_path = "/v1/getticker"
+        return self.request(request_path, param = param)
+
+    def get_chats(self, **param):
+        """
+        GET Public HTTPRequest
+        get the a list of message of chat log
+
+        Parameter:
+        --------------
+        from_date: 2016-02-15
+
+        Return
+        --------------
+        HTTPRequest of tornado.httpclient
+        """
+        request_path = "/v1/getchats"
+        return self.request(request_path, param = param)
+
+    def get_balance(self, **param):
+        """
+        GET Public HTTPRequest
+        get a list of balances sheet of user
+
+        Parameter:
+        --------------
+        from_date: 2016-02-15
+
+        Return
+        --------------
+        HTTPRequest of tornado.httpclient
+        """        
+        request_path = "/v1/me/getbalance"
+        return self.request(request_path, param = param)
+
+    def get_collateral(self, **param):
+        """
+        GET Public HTTPRequest
+        get a list of user collateral statue
+
+        Parameter:
+        --------------
+        param
+
+        Return
+        --------------
+        HTTPRequest of tornado.httpclient
+        """        
+        request_path = "/v1/me/getcollateral"
+        return self.request(request_path, param = param)
+
+    def send_order(self, product_code, price, size, minute_to_expire=100000, 
+                   child_order_type="LIMIT", side="BUY", time_in_force="GTC"):
+        """
+        POST a child order based on your parameter.
+
+        Parameter:
+        ---------------
+        "product_code": product_code,
+        "child_order_type": child_order_type,
+        "side": side, 
+        "price": price, (default)
+        "size": size, (default)
+        "minute_to_expire": minute_to_expire, (default)
+        "time_in_force": time_in_force (default)
+
+        Return:
+        ---------------
+        HTTPRequest of tornado.httpclient 
+        """
         param = {
             "product_code": product_code,
             "child_order_type": child_order_type,
@@ -63,29 +189,82 @@ class bitflyerApi(object):
         }
         request_path = "/v1/me/sendchildorder"
         return self.request(request_path, method = "POST", param = param)
+    
     def get_order(self, **param):
-        print(param)
+        """
+        GET Private HTTPRequest 
+
+        Parameter:
+        -------------
+        product_code: "BTC_JPY"
+        count: 100
+        before: 0
+        after: 0
+        child_order_state: "ACTIVE"
+        child_order_id: "JFX20171111-120000-421212"
+        child_order_accepta_:"JFX20171111-120000-421212"
+        parent_order_id: ""
+
+        Return:
+        --------------
+        HTTPRequest of tornado.httpclient
+        """
         request_path = "/v1/me/getchildorders"
         return self.request(request_path, param = param)
-"""
-@tornado.gen.coroutine
-def json_fetch(http_client):
-    response = yield http_client.fetch("http://api.bitflyer.jp/v1/getmarkets", method = 'GET')
-    raise gen.Return(response)
 
-@tornado.gen.coroutine
-def request():
-    http_client = tornado.httpclient.AsyncHTTPClient()
-    http_response = yield json_fetch(http_client)
-    print(http_response.body)
+    def get_board(self, **param):
+        """
+        GET Public HTTPRequest
 
-if __name__ == "__main__":
-    #tornado.options.parse_command_line()     
-    time_before_exec = datetime.now()
-    instance = bitflyerApi(argv[1], argv[2])
-    instance.get_order(product_code="FX_BTC_JPY", child_order_state="ACTIVE")
-    #instance.send_order("FX_BTC_JPY", 2500000, 0.1, side = "SELL")
-    #tornado.ioloop.IOLoop.instance().run_sync(request)
-    time_after_exec = datetime.now()
-    print(time_after_exec - time_before_exec)
-    """
+        Parameter: (option)
+        --------------
+        product_code: "FX_BTC_JPY"
+
+        Return: HTTPRequest
+        --------------
+        HTTPRequest of tornado.httpclient
+        """
+        request_path = "/v1/getboard"
+        return self.request(request_path, param = param)
+
+    def send_parentorder(self):
+        """
+        GET Public HTTPRequest
+
+        Parameter: (option)
+        --------------
+        product_code: "FX_BTC_JPY"
+
+        Return: HTTPRequest
+        --------------
+        HTTPRequest of tornado.httpclient
+        """
+        param = {
+        "order_method": "IFDOCO",
+        "minute_to_expire": 10000,
+        "time_in_force": "GTC",
+        "parameters": [{
+            "product_code": "FX_BTC_JPY",
+            "condition_type": "LIMIT",
+            "side": "BUY",
+            "price": 2470000,
+            "size": 0.1
+        },
+        {
+            "product_code": "FX_BTC_JPY",
+            "condition_type": "LIMIT",
+            "side": "SELL",
+            "price": 2570000,
+            "size": 0.1
+        },
+        {
+            "product_code": "FX_BTC_JPY",
+            "condition_type": "STOP_LIMIT",
+            "side": "SELL",
+            "price": 2400000,
+            "trigger_price": 2400001,
+            "size": 0.1
+        }]
+        }        
+        request_path = "/v1/me/sendparentorder"
+        return self.request(request_path,method = "POST", param = param)
--- coroutine.py
+++ coroutine.py
@@ -2,30 +2,21 @@ import os, json, time, tornado.ioloop, tornado.web
 from tornado import gen
 from tornado.httpclient import AsyncHTTPClient, HTTPClient, HTTPRequest, HTTPError
 from bitflyerAPI import bitflyerApi
+from tornado.escape import json_decode, json_encode
+import urllib
 
 http_client = AsyncHTTPClient()
+api = bitflyerApi()
 
 @gen.coroutine
-def loop():
-    param = {
-            "product_code": "FX_BTC_JPY",
-            "child_order_type": "LIMIT",
-            "side": "BUY",
-            "price": 1800000,
-            "size": 0.1,
-            "minute_to_expire": 100000,
-            "time_in_force": "GTC"
-        }
-    data = json.dumps(param)
-    api = bitflyerApi(key="", secret="")
-    head = api.auth_header("/v1/me/sendchildorder","POST",data)
-    request = HTTPRequest("https://api.bitflyer.jp/v1/me/sendchildorder", 'POST', head, body=data)
+def make_order(prices):
+    request = api.send_order(product_code = "FX_BTC_JPY", price = prices, size = 0.1)
     response = yield http_client.fetch(request)
     raise gen.Return(response)
 
 @gen.coroutine
 def research():
-    request = HTTPRequest("https://api.bitflyer.jp/v1/ticker?product_code=FX_BTC_JPY", "GET")
+    request = api.get_board(product_code = "FX_BTC_JPY")
     response = yield http_client.fetch(request)
     raise gen.Return(response)
 
@@ -33,19 +24,78 @@ def research():
 def ready():
     while True:
         response = yield research()
-        research_json = json.loads(response.body)
-        price_one_second_before = research_json["ltp"]
-        #response = yield loop()
-        yield gen.sleep(1)
-        response = yield research()
-        research_json = json.loads(response.body)
-        price_one_second_after = research_json["ltp"]
-        
-        print((price_one_second_before-price_one_second_after)*100000/price_one_second_before)
-        if price > 2220001:
-            response = yield loop()
-            print(response.body)
-            break
+        board = json.loads(response.body)
+        print("The mid price is " + str(board["mid_price"]))
+        list_bids = board["bids"]
+        list_asks = board["asks"]
+        sum_bids = 0
+        sum_asks = 0
+        list_size_bids = []
+        list_size_asks = []
+        for bids in list_bids[:100]:
+            #sum_bids += bids["size"]*bids["price"]
+            list_size_bids.append(bids["size"])
+        for asks in list_asks[:100]:
+            list_size_asks.append(asks["size"])
+
+        print("the max size of bids" + str(list_bids[list_size_bids.index(max(list_size_bids))]))
+        print("the max size of asks" + str(list_asks[list_size_asks.index(max(list_size_asks))]))
+
+        max_size_of_price = list_bids[list_size_bids.index(max(list_size_bids))]
+        if max_size_of_price["size"] >= 100:
+            response_make_order = yield make_order(max_size_of_price["price"])
+            if response_make_order.body:
+                print(json.loads(response_make_order.body))
+                break 
+        yield gen.sleep(120)
+
+@gen.coroutine
+def chats():
+    request = api.get_chats(from_date= "2017-12-16T17:16:37.483")
+    response = yield http_client.fetch(request)
+    raise gen.Return(response)
+
+@gen.coroutine
+def search_chats():
+    response = yield chats()
+    response_json = json.loads(response.body)
+    for s in response_json:
+        if s["nickname"] == "G-DRAGON":
+            print(s["message"])
+
+@gen.coroutine
+def balance():
+    request = api.get_balance()
+    response = yield http_client.fetch(request)
+    raise gen.Return(response)
+
+@gen.coroutine
+def get_balance():
+    response = yield balance()
+    print(json.loads(response.body))
+
+@gen.coroutine
+def collateral():
+    request = api.get_collateral()
+    response = yield http_client.fetch(request)
+    raise gen.Return(response)
+
+@gen.coroutine
+def get_collateral():
+    response = yield collateral()
+    print(json.loads(response.body))
+
+@gen.coroutine
+def parentorder():
+    request = api.send_parentorder()
+    response = yield http_client.fetch(request)
+    raise gen.Return(response)
+
+@gen.coroutine
+def send_parentorder():
+    response = yield parentorder()
+    print(json.loads(response.body))    
+
 if __name__ == "__main__":
     aap = tornado.ioloop.IOLoop.current()
     tornado.ioloop.IOLoop.instance().run_sync(ready)
